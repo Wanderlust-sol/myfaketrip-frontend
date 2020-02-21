2927caaa3697e79b1916c325b12c4e67092d19e8
Taesol Kwon <ds3bbbb@naver.com>
2020-02-21 15:35:09 +0900

    initial settings

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        <div>어디로 떠나세요?</div>
+      </div>
+    );
+  }
+}
+
+export default Body;
