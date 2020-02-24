297ee2df8dd71ed9242e47237a387ac001bca804
Taesol Kwon <ds3bbbb@naver.com>
2020-02-24 14:21:28 +0900

    재수정

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../../styles/Common.scss";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div className="login_area">
+          <div className="login_container">
+            <ul></ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
